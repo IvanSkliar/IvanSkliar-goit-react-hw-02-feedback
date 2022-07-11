0170d87e76641ca7dfc444dea8af1c9621b795e0
IvanSkliar <skliariv91@gmail.com>
2022-07-11 14:14:57 +0300

    dz 2 / 4

---
+++ src/App.jsx
@@ -0,0 +1,65 @@
+import { Component } from 'react';
+
+import Container from './components/Container/Conteiner';
+import FeedbackOptions from './components/FeedbackOptions/FeedbackOptions';
+import Statistics from './components/Statistics/Statistics';
+import Section from './components/Section/Section';
+
+class App extends Component {
+    state = {
+        good: 0,
+        neutral: 0,
+        bad: 0,
+    };
+
+    getOptions = () => {
+        const keys = Object.keys(this.state);
+        return keys.map(
+            key => `${key.slice(0, 1).toUpperCase()}${key.slice(1)}`,
+        );
+    };
+
+    handleChange = ({ target }) => {
+        const feedbackOption = target.textContent.toLowerCase();
+        this.setState({ [feedbackOption]: this.state[feedbackOption] + 1 });
+    };
+
+    countTotalFeedback = () => {
+        const values = Object.values(this.state);
+        return values.reduce((total, value) => total + value, 0);
+    };
+
+    countPositiveFeedbackPercentage = () => {
+        const { good } = this.state;
+        const totalCount = this.countTotalFeedback();
+        return Math.round((good / totalCount) * 100);
+    };
+
+    render() {
+        const options = this.getOptions();
+        const { good, neutral, bad } = this.state;
+
+        return (
+            <Container>
+                <Section title="Please leave feedback">
+                    <FeedbackOptions
+                        options={options}
+                        onLeaveFeedback={this.handleChange}
+                    />
+                </Section>
+
+                <Section title="Statictics">
+                    <Statistics
+                        good={good}
+                        neutral={neutral}
+                        bad={bad}
+                        total={this.countTotalFeedback()}
+                        positivePercentage={this.countPositiveFeedbackPercentage()}
+                    />
+                </Section>
+            </Container>
+        );
+    }
+}
+
+export default App;
